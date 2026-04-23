998f4906aed479fe539ea8e7d76f1010a8c06309
dhuv patel <dhruv.patel@qable.io>
2026-04-23 14:26:53 +0530

    test1 Branch

--- tests/spec/LoginMethod.spec.js
+++ tests/spec/LoginMethod.spec.js
@@ -12,13 +12,4 @@ test.describe('Login Tests', () => {
     await loginPage.runInvalidUsernameTest();
   });
 
-  test('Test case 3: Negative password test', async ({ page }) => {
-    const loginPage = new LoginMethod(page);
-    await loginPage.runInvalidPasswordTest();
-  });
-  test('Test case 4: Again Login Test', async ({ page }) => {  
-    const loginPage = new LoginMethod(page);
-    await loginPage.runInvalidUsernameTest();
-  });
-
 });
